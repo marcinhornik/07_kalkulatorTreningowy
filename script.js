79f76f491a0d921c33660787d6b5d545c5a480a5
MarcinH <horas@o2.pl>
2018-01-03 22:04:56 +0100

    poprawki

--- script.js
+++ script.js
@@ -1,28 +1,61 @@
 let dataTable = [0];
 let numberTable = [0];
 let func = [0];
+let NumberToCount= [0];
+
+
+
 
 function pressNumber(number){
 
+if (document.getElementById("viewToDoAction1").value === "")
 
-
+{
 dataTable[number] = number;
 document.getElementById("viewToDoAction").innerHTML += dataTable[number] ;
 
-numberTable[1] = document.getElementById("viewToDoAction").value;
-console.log(numberTable[1]);
+numberTable[1] = Number(document.getElementById("viewToDoAction").value);
+}
 
+else {
+dataTable[number] = number;
+document.getElementById("viewToDoAction2").innerHTML += dataTable[number] ;
+
+numberTable[2] = Number(document.getElementById("viewToDoAction2").value);
+}
+
+
+console.log(numberTable);
+}
+
+function action(action){
+
+
+NumberToCount[NumberToCount.length]= numberTable[1];
+
+let actionNo = [0,"+","-","*","/"];
+
+numberTable[2] = actionNo[action];
+
+
+document.getElementById("viewToDoAction1").innerHTML += actionNo[action]
 
 }
 
-function plus(){
-func[1] = document.getElementById("buttonPlus").value;
-document.getElementById("viewToDoAction").innerHTML += func[1] ;
-}
 
 function equal(){
-console.log(dataTable);
 
-document.getElementById("viewResult").innerHTML = numberTable[1]+dataTable[2];
+if (document.getElementById("viewToDoAction1").value === "+") {document.getElementById("viewResult").innerHTML = numberTable[1]+numberTable[2];}
 
+
+else if (document.getElementById("viewToDoAction1").value === "-") {document.getElementById("viewResult").innerHTML = numberTable[1]-numberTable[2];}
+else if (document.getElementById("viewToDoAction1").value === "*") {document.getElementById("viewResult").innerHTML = numberTable[1]*numberTable[2];}
+else if (document.getElementById("viewToDoAction1").value === "/") {document.getElementById("viewResult").innerHTML = numberTable[1]/numberTable[2];}
+console.log(numberTable);
+}
+
+function clearr(){
+
+location.reload();  //przeładowuje strone i resetuje dane w zmiennych
+console.log("clear");
 }
